97b8ad08495e5f65ef5cb0b151aafa6c1c0b61b0
JahongirFox <abdurashidovabdulatif605@gmail.com>
2025-01-28 19:48:20 +0500

    Chnaged the Navabr responsive

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,14 +3,14 @@ import './Home.css'
 function Home() {
     return (
             <div className="Home">
-            
+{/*             
                 <p className="Home-p">Закажите качественные моторные <br /> масла для своего производства</p>
                 <div className="Home-Center">
                     <div className="flex-btn">
                         <button className="home-btn">Перейти к выбору</button>   
                         <button className="home-btn2">Связаться с нами</button>
                     </div>
-                </div>
+                </div> */}
 
             </div>
 
